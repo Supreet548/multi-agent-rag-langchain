44b4ec671e5ed3a1a0f41dad137c766250349743
Supreet Kumar <supreetkumar548@gmail.com>
2026-06-01 12:22:00 +0530

    Fixed model folder issue .env file not loaded

--- app/models/embedding.py
+++ app/models/embedding.py
@@ -2,6 +2,9 @@ import os
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
+from dotenv import load_dotenv
+load_dotenv()
+
 embeddings = GoogleGenerativeAIEmbeddings(
     model=os.getenv("MODEL_EMBED"),
     google_api_key=os.getenv("GOOGLE_API_KEY")
--- app/models/llm.py
+++ app/models/llm.py
@@ -2,6 +2,10 @@ import os
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 llm = ChatGoogleGenerativeAI(
     model=os.getenv("MODEL_CHAT"),
     google_api_key=os.getenv("GOOGLE_API_KEY"),
